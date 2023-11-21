5ac1087c5ad9858e9efc7a1552d6206752ec8319
Monica Grevious <monica.grevious@gmail.coml>
2023-11-21 10:39:13 -0500

    Styling applied, js removed.

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+const defaultTheme = require("tailwindcss/defaultTheme");
+
+module.exports = {
+  content: ["./**/*.html"],
+  theme: {
+    extend: {
+      spacing: {
+        128: "32rem",
+      },
+      fontFamily: {
+        sans: ["Hanken\\ Grotesk", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        primary: "#CAC9FF",
+        secondary: "#2F2CE9",
+        rosered: { 200: "#FFF6F6", 600: "#FF5555" },
+        mustard: { 200: "#FFFBF4", 600: "#FFB21E" },
+        darknavy: "#303B59",
+        grass: {
+          200: "#F2FCF9",
+          600: "#00BB8F",
+        },
+        cobalt: { 200: "#F3F4FD", 600: "#1125D6" },
+      },
+      fontSize: { "3xl": "2rem" },
+    },
+  },
+  plugins: [],
+};
